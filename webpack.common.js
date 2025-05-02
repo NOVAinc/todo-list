1270f3b3bf74c3068ea76506426efecc684536ef
Lucas Medina <19545604+NOVAinc@users.noreply.github.com>
2025-05-02 00:01:59 -0300

    Initial webpack configuration

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|apng|bpm|webp|svg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
