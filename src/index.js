69aab50a705082eb8899e981ccbfc3747e80d483
Lucas Medina <19545604+NOVAinc@users.noreply.github.com>
2025-05-06 02:55:33 -0300

    Initial index setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import "./styles.css";
+import ProjectManager from "./project-manager.js";
+import Project from "./project.js";
+import Todo from "./todo.js";
+
+let defaultProject = new Project("default");
+ProjectManager.add(defaultProject);
+defaultProject.add(new Todo("default", "", new Date(), "Normal"));
