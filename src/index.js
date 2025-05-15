7810dc26c3f5ed46ce0a547062dc6d605cdf607b
Lucas Medina <19545604+NOVAinc@users.noreply.github.com>
2025-05-15 03:27:31 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import Todo from "./todo.js";
 import DOMManager from "./dom-manager.js";
 import "./styles.css";
 
-const ACTIVE_PROJECT = null;
-
 // Start the project manager, including fetching locally stored projects
 ProjectManager.initialize();
 
